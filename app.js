a0245c2c1fb2f660a9f357e6f832886a1c857c66
jrandolph614 <jrandolph614@gmail.com>
2019-01-09 21:53:50 -0500

    new page

---
+++ app.js
@@ -0,0 +1,23 @@
+var parseTime = d3.timeParse("%m, %d, %Y")
+d3.csv("teslanew.csv", function(data) {
+    teslaData.forEach(function(data) {
+        data.open_price = +data.open_price;
+        data.high_price = +data.high_price;
+        data.low_price = +data.low_price;
+        data.closing_price = +data.closing_price;
+        data.adj_close = +data.adj_close;
+        data.volume = +data.volume;
+        data.range = +data.range;
+        data.rng_perct_close = +data.rng_perct_close;
+        data.real_date =  parseTime(data.real_date)
+    });
+    d3.select("tbody")
+    .selectAll("tr")
+    .data(teslaData)
+    .enter()
+    .append("tr")
+    .html(function(d){
+        return `<td>${d.real_date}</td><td>${d.open_price}</td><td>${d.high_price}</td><td>${d.low_price}</td><td>${d.closing_price}</td><td>${d.adj_close}</td>`+
+        `<td>${d.volume}</td><td>${d.range}</td><td>${d.rng_perct_close}</td>`
+    });
+});
